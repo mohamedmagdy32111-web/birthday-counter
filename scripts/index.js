172361011a67222bf79294182e118f83045c0c3b
mohamedmagdy32111-web <mohamedmagdy32111@gmail.com>
2026-09-03 00:44:11 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
-  name: 'Darlene'
+  birthdate: 'Sep 15, 2026',
+  name: 'Sohila Hesham'
 };
 
 function hideEverything() {
